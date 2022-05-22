89762ab285e8f3de9b4a8c66f6dad9d4fc715e2d
alexdemoraes <alexdemoraes@gmail.com>
2022-05-21 21:14:49 -0300

    Ajustando para rodar no CMD

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,7 +1,9 @@
+@echo off
 set FLASK_APP=./controlehoras/index.py
 REM Essa variável permite que a aplicação recarregue toda vez que a aplicação mudar
 set FLASK_ENV=development
 
-source $(pipenv --venv)/bin/activate
+for /f usebackq %%V in (`pipenv --venv`) do set VENV=%%V
+call %VENV%\scripts\activate.bat
 
 flask run -h 0.0.0.0
